e6cb5c3239303743ff88dc16541fed67bc3abe6c
xDouglas90 <xdouglas90@gmail.com>
2022-09-10 17:30:16 -0300

    :sparkles: feat: Create NavBar molecule component #5

---
+++ src/components/molecules/NavBar/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const NavBar = styled.nav`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 0px;
+  gap: 20px;
+
+  width: 298px;
+  height: 31px;
+`;
