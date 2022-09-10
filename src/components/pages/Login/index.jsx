1e27e4926161afaaffb4b060bfd03000cd04d09e
xDouglas90 <xdouglas90@gmail.com>
2022-09-10 20:22:43 -0300

    :sparkles: feat: Create Login page #9

---
+++ src/components/pages/Login/index.jsx
@@ -0,0 +1,28 @@
+import { Button, Input, Link, Title } from '@atoms';
+import { Layout } from '@templates';
+
+import * as S from './styles';
+
+export const Login = () => {
+  return (
+    <Layout>
+      <S.Container>
+        <Title text="Acessar" />
+        <Input
+          label="E-mail"
+          name="email"
+          placeholder="Seu e-mail"
+          type="email"
+        />
+        <Input
+          label="Senha"
+          name="password"
+          placeholder="Sua senha"
+          type="password"
+        />
+        <Button isPrimary text="Acessar" />
+        <Link text='Cadastrar' url='#' />
+      </S.Container>
+    </Layout>
+  );
+};
