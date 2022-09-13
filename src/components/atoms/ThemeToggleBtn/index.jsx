d91d50b893b2c7a6631c3cfdd0aa013fbfa275e5
xDouglas90 <xdouglas90@gmail.com>
2022-09-13 08:28:41 -0300

    :sparkles: feat: Create theme toggle button atom component #15

---
+++ src/components/atoms/ThemeToggleBtn/index.jsx
@@ -0,0 +1,30 @@
+import { useContext } from 'react';
+import PropTypes from 'prop-types';
+import Switch from 'react-switch';
+
+import { ThemeContext } from 'styled-components';
+import { shade, lighten } from 'polished';
+import * as S from './styles';
+
+export const ThemeToggleBtn = ({ theme, toggleTheme }) => {
+  const { colors } = useContext(ThemeContext);
+
+  return (
+    <Switch
+      onChange={toggleTheme}
+      checked={theme}
+      height={30}
+      width={80}
+      handleDiameter={30}
+      offColor={shade(0.15, colors.primary)}
+      onColor={lighten(0.2, colors.primary)}
+      uncheckedIcon={<S.DarkIcon />}
+      checkedIcon={<S.LightIcon />}
+    />
+  );
+};
+
+ThemeToggleBtn.propTypes = {
+  theme: PropTypes.object.isRequired,
+  toggleTheme: PropTypes.func.isRequired,
+};
--- src/components/templates/Layout/index.jsx
+++ src/components/templates/Layout/index.jsx
@@ -1,19 +1,13 @@
-import { useContext } from 'react';
 import PropTypes from 'prop-types';
-import Switch from 'react-switch';
 
 import { useCustomTheme } from '@contexts';
 
 import { TopBar } from '@organisms';
-import { Button, Logo } from '@atoms';
+import { Button, Logo, ThemeToggleBtn } from '@atoms';
 
-import { shade, lighten } from 'polished';
-
-import { ThemeContext } from 'styled-components';
 import * as S from './styles';
 
 export const Layout = ({ children }) => {
-  const { colors } = useContext(ThemeContext);
   const { theme, toggleTheme } = useCustomTheme();
 
   return (
@@ -21,18 +15,11 @@ export const Layout = ({ children }) => {
       <TopBar>
         <Logo />
         <S.Navigation>
-        <Button text="Login" />
-        <Switch
-          onChange={toggleTheme}
-          checked={theme.title === 'Claro'}
-          checkedIcon={false}
-          uncheckedIcon={false}
-          height={20}
-          width={40}
-          handleDiameter={20}
-          offColor={shade(0.15, colors.primary)}
-          onColor={lighten(0.2, colors.primary)}
-        />
+          <Button text="Login" />
+          <ThemeToggleBtn
+            toggleTheme={toggleTheme}
+            theme={theme.title === 'Claro'}
+          />
         </S.Navigation>
       </TopBar>
       <S.Main>{children}</S.Main>
