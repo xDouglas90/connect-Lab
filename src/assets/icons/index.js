918dac3cc2504561418c6ebdec50eb8d0956273c
odouglas.dev <odouglas.dev@gmail.com>
2022-09-16 18:15:51 -0300

    :sparkles: feat: Create Select atom component #27

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,5 +2,6 @@ import moonIcon from './moon.png';
 import offIcon from './off-icon.svg';
 import onIcon from './on-icon.svg';
 import sunIcon from './sun.png';
+import downArrowIcon from './down-arrow.png';
 
-export { moonIcon, offIcon, onIcon, sunIcon };
+export { downArrowIcon, moonIcon, offIcon, onIcon, sunIcon };
