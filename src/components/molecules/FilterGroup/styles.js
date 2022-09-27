5ed46bef5ed2fc87606f53c44a72f11ed2199b4b
xDouglas90 <xdouglas90@gmail.com>
2022-09-27 07:16:39 -0300

    :ok_hand: style: Fix FilterGroup responsiveness #41

--- src/components/molecules/FilterGroup/styles.js
+++ src/components/molecules/FilterGroup/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const BtnGroup = styled.div`
   display: flex;
+  flex-wrap: wrap;
   align-items: center;
   justify-content: flex-start;
   gap: 1rem;
